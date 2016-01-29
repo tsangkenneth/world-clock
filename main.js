7bcab87e17336f58f6533a3616649783ded72873
Kenneth Tsang <kenneth@registria.com>
2016-01-28 17:10:07 -0800

    Revise for loop for timezones to work with Chrome

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ window.onload = function main() {
 
   var getCalendarOffset = function getCalendarOffset(timezone) {
     var localTime = moment.tz(moment().format('YYYY-MM-DD, HH:mm:ss'), 'America/Los_Angeles');
-    
+
     return moment.tz(timezone).calendar(localTime, {
       sameDay: '[Today]',
       nextDay: '[Tomorrow]',
@@ -15,7 +15,9 @@ window.onload = function main() {
   }
 
   var updateTimes = function updateTimes() {
-    for (var timezone of timezones) {
+    for (var i = 0, length = timezones.length; i < length; i++) {
+      var timezone = timezones[i];
+
       timezone.getElementsByClassName('timezone__time')[0].textContent = moment.tz(timezone.dataset.timezone).format('HH:mm:ss z');
       timezone.getElementsByClassName('timezone__offset')[0].textContent = getCalendarOffset(timezone.dataset.timezone);
     }
